716aa42778c1a333697e14125727ee5a43a3b17c
Ben Pearey <bpearey@amplience.com>
2024-02-22 19:20:13 +0000

    fix: using skeleton components for curated and product grid until we have an api route for getProducts

--- components/amplience/curated-product-grid/curated-product-grid.tsx
+++ components/amplience/curated-product-grid/curated-product-grid.tsx
@@ -1,27 +1,31 @@
-import { getProducts } from '~/client/queries/get-products';
-import { ProductCardCarousel } from '~/components/product-card-carousel';
+// import { getProducts } from '~/client/queries/get-products';
+// import { ProductCardCarousel } from '~/components/product-card-carousel';
 
-interface CuratedProductGridProps {
-  header: string;
-  products: string[];
-}
+// interface CuratedProductGridProps {
+//   header: string;
+//   products: string[];
+// }
 
-const CuratedProductGrid = async ({ header, products }: CuratedProductGridProps) => {
-  const productIds = products.map((item) => Number(item));
-  const prods = await getProducts({
-    productIds,
-    first: productIds.length,
-  });
+// const CuratedProductGrid = async ({ header, products }: CuratedProductGridProps) => {
+//   const productIds = products.map((item) => Number(item));
+//   const prods = await getProducts({
+//     productIds,
+//     first: productIds.length,
+//   });
 
-  return (
-    <ProductCardCarousel
-      products={prods}
-      showCart={false}
-      showCompare={false}
-      showReviews={false}
-      title={header}
-    />
-  );
+//   return (
+//     <ProductCardCarousel
+//       products={prods}
+//       showCart={false}
+//       showCompare={false}
+//       showReviews={false}
+//       title={header}
+//     />
+//   );
+// };
+
+const CuratedProductGrid = () => {
+  return <p>CuratedProductGrid</p>;
 };
 
 export default CuratedProductGrid;
