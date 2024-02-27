b5cb152d061e84fe059e2fcbadde3cde64e733c6
Ben Pearey <bpearey@amplience.com>
2024-02-27 16:49:14 +0000

    feat: enable preview on the homepage and persist query string on link compoenent navigation

--- app/amplience/visualization/page.tsx
+++ app/amplience/visualization/page.tsx
@@ -1,23 +1,24 @@
+import { ReadonlyURLSearchParams } from 'next/navigation';
+
 import { createAmplienceClient } from '~/amplience-client';
+import { clientOptionsMapper } from '~/amplience-client/mappers/client-options-mapper';
 import RealtimeVisualization from '~/components/amplience/realtime-visualization/realtime-visualization';
 
 export interface VisualizationProps {
-  searchParams: {
-    hubName: string;
-    locale: string;
-    stagingEnvironment: string;
-    contentId: string;
-  };
+  searchParams: ReadonlyURLSearchParams & { contentId: string };
 }
 
 export default async function Visualization({ searchParams }: VisualizationProps) {
-  const { hubName, locale, stagingEnvironment, contentId } = searchParams;
-
-  const amplienceClient = createAmplienceClient({ hubName, locale, stagingEnvironment });
-
+  const amplienceClientOptions = clientOptionsMapper(searchParams);
+  const amplienceClient = createAmplienceClient(amplienceClientOptions);
+  const { contentId } = searchParams;
   const content = await amplienceClient.getContentItemById(contentId);
 
-  return <div>{content && <RealtimeVisualization content={content} />}</div>;
+  return (
+    <div>
+      <RealtimeVisualization content={content} />
+    </div>
+  );
 }
 
 export const runtime = 'edge';
