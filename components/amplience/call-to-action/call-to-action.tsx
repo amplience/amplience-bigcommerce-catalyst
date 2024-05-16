649019a0eca9eaabc300024a64ae3e2edcdf40de
Réza Kalfane <reza.kalfane@gmail.com>
2024-05-16 11:43:34 +0200

    fix: styling

--- components/amplience/call-to-action/call-to-action.tsx
+++ components/amplience/call-to-action/call-to-action.tsx
@@ -19,14 +19,7 @@ const CallToAction = ({
   return href ? (
     <Link
       href={href}
-      className={clsx(
-        `font mt-4 rounded bg-[#333] px-3.5 py-2.5 text-xs font-bold text-[#eee] no-underline hover:bg-[#eee] hover:text-[#333] hover:no-underline`,
-        {
-          ['font mt-4 rounded bg-[#eee] px-3.5 py-2.5 text-xs font-bold text-[#333] no-underline hover:bg-[#333] hover:text-[#eee] hover:no-underline']:
-            variant === 'contained',
-        },
-        className,
-      )}
+      className="font mt-4 rounded bg-[#333] px-3.5 py-2.5 text-xs font-bold text-[#eee] no-underline hover:bg-[#eee] hover:text-[#333] hover:no-underline"
       {...other}
     >
       {children}
