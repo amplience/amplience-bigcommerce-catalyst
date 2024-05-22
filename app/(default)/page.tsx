38cb82768912433d89329f201ffd3e8a6e25456f
Réza Kalfane <reza.kalfane@gmail.com>
2024-05-22 15:25:43 +0200

    fix: updated get all items code

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -4,7 +4,6 @@ import { ReadonlyURLSearchParams } from 'next/navigation';
 import { createAmplienceClient } from '~/amplience-client';
 import { clientOptionsMapper } from '~/amplience-client/mappers/client-options-mapper';
 import AmplienceContent from '~/components/amplience/wrapper/amplience-content';
-
 const FLEXIBLE_SLOT_KEY = String(process.env.AMPLIENCE_FLEXIBLE_SLOT_KEY);
 
 export interface HomeProps {
@@ -17,17 +16,18 @@ export default async function Home({ searchParams }: HomeProps) {
   const [flexibleSlot] = await Promise.all([
     (await amplienceClient.getContentItemByKey(FLEXIBLE_SLOT_KEY)).toJSON() as DefaultContentBody,
   ]);
-  const allItemIds = flexibleSlot.slots.map((content: any) => content.id);
-  const allItems = (await amplienceClient.getContentItemsById(allItemIds)).responses;
+  const allItemIds: string[] = flexibleSlot.slots.map((content: any) => content.id);
+  const allItems = await amplienceClient.getContentItemsById(allItemIds);
 
   return (
     <>
-      {allItems.map((item: any, index: number) => {
-        return <AmplienceContent content={item.content} key={index} />;
-      })}
+      {allItems &&
+        allItems.responses &&
+        allItems.responses.map((item: any, index: number) => {
+          return <AmplienceContent content={item.content} key={index} />;
+        })}
     </>
   );
-  // return <AmplienceContent content={flexibleSlot} />;
 }
 
 export const runtime = 'edge';
