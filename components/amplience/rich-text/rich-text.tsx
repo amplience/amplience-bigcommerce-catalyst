9dda7381d08c3222302b95cc8dd52406c0d35c9b
Réza Kalfane <reza.kalfane@gmail.com>
2024-06-12 16:11:17 +0200

    fix: text formatting

--- components/amplience/rich-text/rich-text.tsx
+++ components/amplience/rich-text/rich-text.tsx
@@ -24,6 +24,48 @@ const RichText = ({ text = [], align = 'left' }: RichTextProps & DefaultContentB
   const options = {
     overrides: {
       a: { component: Link },
+      h1: {
+        component: ({ ...props }) => (
+          <h1 className="mb-4 mt-4 text-3xl font-black lg:text-5xl">
+            <span {...props} />
+          </h1>
+        ),
+      },
+      h2: {
+        component: ({ ...props }) => (
+          <h2 className="mb-4 mt-4 text-2xl font-black lg:text-4xl">
+            <span {...props} />
+          </h2>
+        ),
+      },
+      h3: {
+        component: ({ ...props }) => (
+          <h3 className="mb-4 mt-4 text-xl font-black lg:text-3xl">
+            <span {...props} />
+          </h3>
+        ),
+      },
+      h4: {
+        component: ({ ...props }) => (
+          <h4 className="mb-4 mt-4 text-xl font-black lg:text-2xl">
+            <span {...props} />
+          </h4>
+        ),
+      },
+      h5: {
+        component: ({ ...props }) => (
+          <h5 className="mb-4 mt-4 text-xl font-black lg:text-xl">
+            <span {...props} />
+          </h5>
+        ),
+      },
+      h6: {
+        component: ({ ...props }) => (
+          <h5 className="mb-4 mt-4 font-black">
+            <span {...props} />
+          </h5>
+        ),
+      },
       li: {
         component: ({ ...props }) => (
           <li>
